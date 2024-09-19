2bd72c34807f24d2ff93c903fc2a268e3d6bffe3
Varad Sharma <varad.code19@gmail.com>
2024-09-20 00:59:21 +0530

    content added

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,22 @@ function App() {
     // The Main Content
     <div className="letter-div-body">
       {/* The Main Title */}
-      <h1>Lorem Ipsum</h1>
+      <p>My Love</p>
+      <p>
+        I was planning to write this as a physical letter in reply to the ones
+        you’ve given me, but since it’s your birthday, I thought I’d make it a
+        little present instead—it's the least I can do (plus, I needed an excuse
+        to write a bit of code, haha). To start off, I never thought I’d be with
+        someone again, but after meeting you, I can't imagine being with anyone
+        else. We may not have everything figured out, but we’ve got time, and
+        I’m confident we will. I know I’m not the best at expressing myself or
+        communicating, but we’ll figure that out too. Let’s figure life out
+        together and turn this beautiful mess into our own masterpiece. There’s
+        a lot more I wanted to say, but I’ll save it for another day. I love
+        you, Aanya. Happy birthday!
+        <br />
+      </p>
+      <p>Varad</p>
     </div>
   );
 }
